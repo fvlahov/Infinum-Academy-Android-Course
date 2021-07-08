c9d6230fad3a871ab4fdb47e12af4864be5898cf
fvlahov <franko.vlahov@gmail.com>
2021-07-08 22:09:41 +0200

    Add login activity and main activity

--- app/src/main/java/hr/fvlahov/shows_franko_vlahov/MainActivity.kt
+++ app/src/main/java/hr/fvlahov/shows_franko_vlahov/MainActivity.kt
@@ -1,11 +1,31 @@
 package hr.fvlahov.shows_franko_vlahov
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hr.fvlahov.shows_franko_vlahov.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val EXTRA_EMAIL = "EXTRA_EMAIL"
+
+        fun buildIntent(activity: Activity, email: String) : Intent {
+            val intent = Intent(activity, MainActivity::class.java)
+            intent.putExtra(EXTRA_EMAIL, email)
+            return intent
+        }
+    }
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val root = binding.root
+
+        setContentView(root)
     }
 }
