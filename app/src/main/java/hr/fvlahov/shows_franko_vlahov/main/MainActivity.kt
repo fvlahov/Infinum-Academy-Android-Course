9702e1d3d77202545330039c61ea576445d6603b
fvlahov <franko.vlahov@gmail.com>
2021-07-17 13:21:38 +0200

    Add MainActivity

---
+++ app/src/main/java/hr/fvlahov/shows_franko_vlahov/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package hr.fvlahov.shows_franko_vlahov.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hr.fvlahov.shows_franko_vlahov.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
