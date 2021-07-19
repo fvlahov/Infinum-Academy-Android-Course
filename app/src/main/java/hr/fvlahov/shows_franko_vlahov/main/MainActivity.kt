80697f37e9c8d7419d2006adbbd9c5c13295f3fe
fvlahov <franko.vlahov@gmail.com>
2021-07-19 11:08:42 +0200

    Started adding support for tablet

--- app/src/main/java/hr/fvlahov/shows_franko_vlahov/main/MainActivity.kt
+++ app/src/main/java/hr/fvlahov/shows_franko_vlahov/main/MainActivity.kt
@@ -2,11 +2,20 @@ package hr.fvlahov.shows_franko_vlahov.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.findFragment
 import hr.fvlahov.shows_franko_vlahov.R
+import hr.fvlahov.shows_franko_vlahov.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
     }
 }
